4ecb26154c21a79adbb858018573dd1528fb1636
Karim Kabirov <karim3487@gmail.com>
2024-04-16 13:47:28 +0300

    Fix slider and banners roll

--- src/components/NewsFilters/NewsFilters.jsx
+++ src/components/NewsFilters/NewsFilters.jsx
@@ -3,6 +3,7 @@ import Categories from "../Categories/Categories.jsx";
 import Search from "../Search/Search.jsx";
 import { useFetch } from "../../helpers/hooks/useFetch.js";
 import { getCategories } from "../../api/apiNews.js";
+import Slider from "../Slider/Slider.jsx";
 
 const NewsFilters = ({ filters, changeFilter }) => {
   const { data: dataCategories } = useFetch(getCategories);
@@ -10,11 +11,15 @@ const NewsFilters = ({ filters, changeFilter }) => {
   return (
     <div className={styles.filters}>
       {dataCategories ? (
-        <Categories
-          categories={dataCategories.categories}
-          selectedCategory={filters.category}
-          setSelectedCategory={(category) => changeFilter("category", category)}
-        />
+        <Slider>
+          <Categories
+            categories={dataCategories.categories}
+            selectedCategory={filters.category}
+            setSelectedCategory={(category) =>
+              changeFilter("category", category)
+            }
+          />
+        </Slider>
       ) : null}
 
       <Search
